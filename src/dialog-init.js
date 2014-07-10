941d5c29c113a9a6cc87e73bd7bc3d9457c1c478
John Bender <john.m.bender@gmail.com>
2014-07-10 09:35:20 -0700

    move the try/catch inline so it selects once, add todo

--- src/dialog-init.js
+++ src/dialog-init.js
@@ -1,15 +1,6 @@
 (function( w, $ ){
   var Dialog = w.componentNamespace.Dialog, doc = document;
 
-  var isValidSelector = function(selector) {
-    try {
-      var $element = $(selector);
-    } catch (error) {
-      return false;
-    }
-    return true;
-  };
-
 	$.fn.dialog = function( transbg ){
 		return this.each(function(){
 			var $el = $( this ), dialog = new Dialog( this );
@@ -45,8 +36,17 @@
 			$( doc ).bind( "click", function( e ){
 				var $a = $( e.target ).closest( "a" );
 
-				if( !dialog.isOpen && $a.length && $a.attr( "href" ) && isValidSelector($a.attr( "href" )) ){
-					var $matchingDialog = $( $a.attr( "href" ) );
+				if( !dialog.isOpen && $a.length && $a.attr( "href" ) ){
+
+					// catch invalid selector exceptions
+					try {
+						var $matchingDialog = $( $a.attr( "href" ) );
+					} catch ( error ) {
+						// TODO should check the type of exception, it's not clear how well
+						//      the error name "SynatxError" is supported
+						return;
+					}
+
 					if( $matchingDialog.length && $matchingDialog.is( $el ) ){
 						$matchingDialog.trigger( Dialog.events.open );
 						e.preventDefault();
