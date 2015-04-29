65e30fdbbcab551382b392a16f878a63bb0efbf3
Jeffrey Lembeck <jlembeck@gmail.com>
2015-04-29 12:11:12 -0700

    Update dependencies

--- dist/dialog.build.js
+++ dist/dialog.build.js
@@ -8,11 +8,12 @@
  * Licensed under the MIT, GPL licenses.
  */
 
+window.jQuery = window.jQuery || window.shoestring;
+
 (function( w, $ ){
 	w.componentNamespace = w.componentNamespace || w;
 
 	var pluginName = "dialog", cl, ev,
-		nullHash = "dialog",
 		doc = w.document,
 		docElem = doc.documentElement,
 		body = doc.body,
@@ -57,7 +58,7 @@
 		this.$background.remove();
 	};
 
-	Dialog.prototype.open = function( e ) {
+	Dialog.prototype.open = function() {
 		if( this.$background.length ) {
 			this.$background[ 0 ].style.height = Math.max( docElem.scrollHeight, docElem.clientHeight ) + "px";
 		}
@@ -75,7 +76,7 @@
 		$html.addClass( cl.open );
 		this.isOpen = true;
 
-		location.hash = this.hash;
+		window.location.hash = this.hash;
 
 		if( doc.activeElement ){
 			this.focused = doc.activeElement;
@@ -113,12 +114,12 @@
 
 		this.$el.trigger( ev.closed );
 	};
-}( this, jQuery ));
+}( this, window.jQuery ));
 
 (function( w, $ ){
   var Dialog = w.componentNamespace.Dialog, doc = document;
 
-	$.fn.dialog = function( transbg ){
+	$.fn.dialog = function(){
 		return this.each(function(){
 			var $el = $( this ), dialog = new Dialog( this );
 
@@ -140,12 +141,12 @@
 					}
 				});
 
-			dialog.$background.bind( "click", function( e ) {
+			dialog.$background.bind( "click", function() {
 				w.history.back();
 			});
 
 			// close on hashchange if open (supports back button closure)
-			$( w ).bind( "hashchange", function( e ){
+			$( w ).bind( "hashchange", function(){
 				var hash = w.location.hash.replace( "#", "" );
 
 				if( hash !== dialog.hash ){
@@ -155,13 +156,15 @@
 
 			// open on matching a[href=#id] click
 			$( doc ).bind( "click", function( e ){
-				var $a = $( e.target ).closest( "a" );
+        var $matchingDialog, $a;
+
+        $a = $( e.target ).closest( "a" );
 
 				if( !dialog.isOpen && $a.length && $a.attr( "href" ) ){
 
 					// catch invalid selector exceptions
 					try {
-						var $matchingDialog = $( $a.attr( "href" ) );
+						$matchingDialog = $( $a.attr( "href" ) );
 					} catch ( error ) {
 						// TODO should check the type of exception, it's not clear how well
 						//      the error name "SynatxError" is supported
@@ -188,4 +191,4 @@
 	$(function(){
 		$( ".dialog" ).dialog();
 	});
-}( this, jQuery ));
+}( this, window.jQuery ));
