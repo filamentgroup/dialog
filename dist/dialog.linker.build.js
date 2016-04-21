410b2b1cb316827fff48b54391f973b3a17909a3
John Bender <john.m.bender@gmail.com>
2016-04-21 13:38:42 -0400

    update assets

--- dist/dialog.linker.build.js
+++ dist/dialog.linker.build.js
@@ -102,6 +102,16 @@ window.jQuery = window.jQuery || window.shoestring;
 			return;
 		}
 
+		// if someone is calling close directly and the hash for this dialog is in
+		// the url then we need to go back, this will trigger the hashchange binding
+		// in init
+		// NOTE the bindings seem better in the constructor e.g.
+		// "#foo".indexOf("foo") === 1
+		if(window.location.hash.replace(/^#/, "") === this.hash){
+			window.history.back();
+			return;
+		}
+
 		this.$el.removeClass( cl.open );
 
 		this.$background.removeClass( cl.bkgdOpen );
@@ -210,13 +220,13 @@ window.jQuery = window.jQuery || window.shoestring;
 				})
 				.bind( "click", function( e ){
 					if( $(e.target).closest(Dialog.selectors.close).length ){
-						w.history.back();
 						e.preventDefault();
+						dialog.close();
 					}
 				});
 
 			dialog.$background.bind( "click", function() {
-				w.history.back();
+				dialog.close();
 			});
 
 			// close on hashchange if open (supports back button closure)
